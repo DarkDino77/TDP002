febcdeb9523fe5714cf404a64a527461a6789b09
Denab905 <denab905@student.liu.se>
2023-08-23 14:02:45 +0200

    Har sparat coden som visar de fyra olika bilderna

---
+++ labb2.py
@@ -0,0 +1,38 @@
+#Första uppgiften online
+'''
+#bild 1
+def main():
+    number = [1,2,3]
+    number_reference=number
+    
+main()
+
+#bild 2
+def main():
+    number = [1,2,3]
+    number_reference=number.copy()
+
+#bild 3     
+def add_element(l, e):
+    l.append(e)
+    return l
+def main():
+    numbers = [1,2,3]
+    add_element(numbers, 4)
+    return numbers
+
+main()
+
+#bild 4
+def add_element(l, e):
+    l=l.copy()
+    l.append(e)
+    return l
+def main():
+    numbers = [1,2,3]
+    add_element(numbers, 4)
+    return numbers
+
+main()
+
+'''
