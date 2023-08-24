e1b795565762a722cfe5a9a964054626e19b803d
phibr608 <phibr608@student.liu.se>
2023-08-24 15:06:14 +0200

    Boilerplate for lab3

---
+++ lab3.py
@@ -0,0 +1,21 @@
+class Card:
+    def __init__(color, name):
+
+        
+    color = ""
+    name  = ""  
+    
+class Deck:
+    def __init__(seed, size):
+        for i in range(size + 1):
+            self.add_card()
+
+    def add_card():
+        
+    
+    def shuffle():
+
+    def move_card():
+        
+        
+    cards = []
