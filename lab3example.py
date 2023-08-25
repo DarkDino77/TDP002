b4ad4f93ed66212cc344a6df6821c4a7ee5de2e7
Denab905 <denab905@student.liu.se>
2023-08-25 16:35:30 +0200

    HAr lekt med lab3exemple

--- lab3example.py
+++ lab3example.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
 import random
 def create_card(value, suit):
     return (value, suit)
@@ -8,23 +10,26 @@ def insert_card(deck, card, position):
 def place_card_on_top(deck, card):
     insert_card(deck, card, 0)
 
-get_card_at_index()
-
 def create_deck():
-    deck =['deck', [] ]
+    deck = ['deck', [] ]
     for suit in range(1, 3):
         #Card number 14 is joker
         for value in range(1, 15):
-            card = create_card(value, suit)
+            
+            if value * 2 == 28:
+                card = create_card(27, suit)
+            elif suit == 2:
+                card = create_card(value + 13, suit)
+            else:
+                card = create_card(value, suit)
             deck[1].append(card)
     return deck
 
 def get_value(card):
-    value = card[1]
-    return card[1]
+    return card[0]
 
 def get_suit(card):
-    return card[0]
+    return card[1]
 
 def display_card(card):
     
@@ -46,13 +51,87 @@ def remove_card(deck, card):
 def move_card(deck, card, position):
     remove_card(deck, card)
     insert_card(deck, card, position)
+    
+def get_card_at_index(deck, index):
+    return deck[1][index-1]
 
 def move_index(deck, index, position):
     card = get_card_at_index(deck, index)
     remove_card(deck, card)
-    insert_card(deck card, position)
+    insert_card(deck, card, position)
 
-def deck_shuffle(deck, seed):
-    random.shuffle(deck[1], seed)
+def deck_shuffle(deck):
+    random.seed(10)
+    random.shuffle(deck[1])
+    
+def index_of_card(deck, card):
+    return deck[1].index(card)
+
+def identify_joker_index_by_suit(deck, suit):
+    for card in deck[1]:
+        if get_value(card) == 27:
+            if get_suit(card) == suit:
+                return index_of_card(deck, card)
+            
+def identify_joker_index(deck):
+    for card in deck[1]:
+        if get_value(card) == 27:
+                return index_of_card(deck, card)
+            
+
+
+def move_index_down_one(deck, index):
+    if (index + 1) > 28:
+        move_index(deck, index, 0)
+    else:
+        move_index(deck, index, index + 1)
+
+def move_joker_down_one(deck, suit):
+    index = identify_joker_index_by_suit(deck, suit)
+    move_index_down_one(deck, index)
+    
+def move_joker_1(deck):
+    move_joker_down_one(deck, 1)
+#Detta är fult
+def move_joker_2(deck):
+    move_joker_down_one(deck, 2)
+    move_joker_down_one(deck, 2)
+    
+def section_of_deck(deck, start, end):
+    return deck[1][start:end]
+
+def remove_index_from_deck(deck, index):
+    del deck[1][index]
+    
+def remove_section_of_deck(deck, start, end):
+    for index in range(start, end):
+        remove_index_from_deck(deck, index)
+    print(deck)
+        
+    
+
+def split_deck_in_3(deck):
+    firstjoker = identify_joker_index(deck)
+    section_A = section_of_deck(deck, 0, firstjoker)
+    remove_section_of_deck(deck, 0, firstjoker)
+    secondjoker = identify_joker_index(deck)
+    section_B = section_of_deck(deck, 0, secondjoker)
+    remove_section_of_deck(deck, 0, secondjoker)
+    section_C = section_of_deck(deck, 0, len(deck[1]))
+    remove_section_of_deck(deck, 0, len(deck[1])-1)
+    hole = section_C + section_B + section_A
+    deck[1].append(hole)
+
+'''    
+def solitaire_keystream(length, deck):
+    deck_shuffle(deck)
+    move_joker_1(deck)
+    move_joker_2(deck)
+    split_deck_in_3(deck)
+    move_cards_down(deck)
+    get_letter(deck)
+     
+
+'''
 
 
