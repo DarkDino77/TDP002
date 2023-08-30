1ce2d6fa859b638277b51677abfd9182aa673e13
Denab905 <denab905@student.liu.se>
2023-08-30 16:42:45 +0200

    Har slipat lite på labb 4

--- lab4.py
+++ lab4.py
@@ -1,17 +1,44 @@
 #create the begining board
+# The board is a list of items where it stores secondary list of tuples
+# where the secondary list position in the first list is the y cordinate
+# And the tuple has the x and peice information 
 def create_board():
-    return [['player',1,1]]
+    return []
 #Adding the difrent things to the board
 def add_to_board(board, x, y, unit):
-    pass
+    board[y].append([(x, unit)])
 def add_wall(board, x, y):
-    add_to_board(board, x, y, "wall")
+    add_to_board(board, x, y, "#")
 def add_box(board, x, y):
-    add_to_board(board, x, y, "box")
-def add_storage(board, x, y):
-    add_to_board(board, x, y, "storage")
+    add_to_board(board, x, y, "o")
+def add_storage_area(board, x, y):
+    add_to_board(board, x, y, ".")
+def add_box_in_storage(board, x, y):
+    add_to_board(board, x, y, "*")
+def add_player_in_storage(board, x, y):
+    add_to_board(board, x, y, "+")
+def creat_player(board, x, y):
+    add_to_board(board, x, y, "@")
+
+
 #
 #Display board
+#A board should be drawn on a upside down XY cordinate system
+#ADT view
+'''
+def get_max_x(boamax(board[][])
+rd):
+def get_symbol(x, y)
+'''
+#player view
+def display_board(board):
+    max_x = get_max_x(board) 
+    max_y = get_max_y(board) 
+    for x in range(1, max_x + 1):
+        for y in range(1, max_y + 1):
+            symbol = get_symbol(x, y) 
+            print(symbol, end="")
+        print()
 #
 #cheack a cordinate on the board
 #
@@ -34,4 +61,9 @@ def add_storage(board, x, y):
 #
 
 board = create_board()
+add_wall(board, 1, 0)
+add_wall(board, 0, 1)
+add_wall(board, 2, 1)
+add_wall(board, 1, 2)
+creat_player(board, 1, 1)
 print(board)
