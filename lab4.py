a539e2925eb40cab0d23515b02eec77691ec03ae
Denab905 <denab905@student.liu.se>
2023-08-30 11:26:40 +0200

    har lekt lite med labb 4

---
+++ lab4.py
@@ -0,0 +1,37 @@
+#create the begining board
+def create_board():
+    return [['player',1,1]]
+#Adding the difrent things to the board
+def add_to_board(board, x, y, unit):
+    pass
+def add_wall(board, x, y):
+    add_to_board(board, x, y, "wall")
+def add_box(board, x, y):
+    add_to_board(board, x, y, "box")
+def add_storage(board, x, y):
+    add_to_board(board, x, y, "storage")
+#
+#Display board
+#
+#cheack a cordinate on the board
+#
+#move player
+#Make your move (a)left, (d)right, (w)up, (s)down: l
+#
+#cheack collision with player
+#
+#move box
+#
+#read file 
+#
+#convert file.txt to board
+#
+#chose a level
+#
+#play game 
+#
+#victory massage 
+#
+
+board = create_board()
+print(board)
