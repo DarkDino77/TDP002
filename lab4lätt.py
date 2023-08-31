2b09d667cd89f8e1f0740890ae2883a9ab50e4f8
Denab905 <denab905@student.liu.se>
2023-08-31 12:11:40 +0200

    Har jobbat på labb 4 orkar inte med hur jag orginelt tänkte på hur board skulle se ut

--- lab4lätt.py
+++ lab4lätt.py
@@ -14,12 +14,7 @@ def create_board():
     return []
 #Adding the difrent things to the board
 def add_to_board(board, x, y, unit):
-    if y > len(board) - 1:
-        board.append([])
-    if x > len(board[y]) - 1:
-        board[y].append((x, unit))
-    else: 
-        board[y].insert(x, (x, unit))
+    board.append((unit, x, y))
 
 def add_wall(board, x, y):
     add_to_board(board, x, y, "#")
@@ -42,38 +37,26 @@ def creat_player(board, x, y):
 
 def get_max_x(board):
     max_x = 0
-    for lines in board:
-        if max_x < len(lines) :
-            max_x = len(lines) 
+    for elements in board:
+        if max_x < elements[1] :
+            max_x = elements[1]
+
     return max_x
 
 def get_max_y(board):
-    return len(board)
+    max_y = 0
+    for elements in board:
+        if max_y < elements[2] :
+            max_y = elements[2] 
+    return max_y
 
 def get_symbol(board, x, y):
-    
-    symbol = ""
-    if board[y] not in board:
-       # print("y")
-        symbol = " "
-    elif board[y][x][0] is not x :
-        #print("x")
-        symbol = " "
-    else:
-        symbol = board[y][x][1]
-        
-    '''
     symbol = " "
-    print(len(board[y])-1, end=" ")
-    print(x, end = "")
-    if len(board[y])-1 < x:
-        symbol = " "
-    elif board[y][x][0] == x  :
-  
-        symbol = board[y][x][1]
-
+    for element in board:
+        if element[1] == x and element[2] == y:
+            symbol = element[0]
+        
     return symbol
-     '''
 
 
 
@@ -82,16 +65,33 @@ def get_symbol(board, x, y):
 def display_board(board):
     max_x = get_max_x(board) 
     max_y = get_max_y(board) 
-    for x in range(max_x):
-        for y in range(max_y):
+    for y in range(max_y+1):
+        for x in range(max_x+1):
             symbol = get_symbol(board, x, y) 
             print(symbol, end="")
-        print("")
 #
 #cheack a cordinate on the board
 #
 #move player
 #Make your move (a)left, (d)right, (w)up, (s)down: l
+#ADT
+def get_player_x(board):
+    for element in board:
+        if element[0] == "@":
+            return element[1]
+def get_player_y(board):
+    for element in board:
+        if element[0] == "@":
+            return element[2]
+#user
+def player_move_left(board):
+    current_x = get_player_x(board)
+    current_y = get_player_y(board)
+
+#def player_move_right(board):
+#def player_move_up(board):    
+#def player_move_down(board):    
+
 #
 #cheack collision with player
 #
@@ -109,10 +109,23 @@ def display_board(board):
 #
 
 board = create_board()
+add_wall(board, 0, 0)
 add_wall(board, 1, 0)
+add_wall(board, 2, 0)
+add_wall(board, 3, 0)
+add_wall(board, 4, 0)
+add_wall(board, 5, 0)
+
 add_wall(board, 0, 1)
-add_wall(board, 2, 1)
+add_wall(board, 0, 2)
 add_wall(board, 1, 2)
+add_wall(board, 2, 2)
+add_wall(board, 3, 2)
+add_wall(board, 4, 2)
+add_wall(board, 5, 2)
+add_wall(board, 5, 1)
+
+add_box(board, 2,1)
+add_storage_area(board, 4,1)
 creat_player(board, 1, 1)
-print(board)
 display_board(board)
